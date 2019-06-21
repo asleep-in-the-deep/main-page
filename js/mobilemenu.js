8e724eee71c96ee3aa8c4355aaabef959cbc0d07
asleep-in-the-deep <47196327+asleep-in-the-deep@users.noreply.github.com>
2019-06-21 19:42:51 +0300

    Update mobilemenu.js

--- js/mobilemenu.js
+++ js/mobilemenu.js
@@ -1,16 +1,28 @@
-function mobileMenu() {
-	var x = document.getElementById('top-nav');
-	if (x.className === "menu") {
-		x.className += "_mobile";
-	} else {
-		x.className = "menu";
-	}
-}
-function menuClose() {
-	var x = document.getElementById('icon');
-	if (x.className === "menu-icon") {
-		x.className += "_close";
-	} else {
-		x.className = "menu-icon";
-	}
-}
+$(document).ready(function() {
+    $(".menu_top-item, .navmenu_down-item, .menu_top-link, .down-link").hover(function() { 
+        $(this).find(".navmenu_down-item").show(400);
+    } , function() { 
+        $(this).find(".navmenu_down-item").hide();
+    });
+});
+
+
+$(document).ready(function() {
+    $(".menu-icon").click(function() {
+        $(this).toggleClass('close-icon');
+    });
+});
+
+$('.menu-icon').click(function() {
+  if ($('#top-nav').hasClass('menu')) {
+    $('#top-nav').removeClass('menu').addClass('menu-mobile');
+  } else {
+    $('#top-nav').removeClass('menu-mobile').addClass('menu');
+  }
+  return false;
+})
+
+$(".menu_top-item").click(function(){
+    $(this).find('.navmenu_down-item').slideToggle();
+    return false;
+});
