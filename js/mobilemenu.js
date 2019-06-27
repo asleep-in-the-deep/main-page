4e24fede2dd68711e3ee4d2b6963cae66521eb46
asleep-in-the-deep <47196327+asleep-in-the-deep@users.noreply.github.com>
2019-06-27 04:02:31 +0300

    Add files via upload

--- js/mobilemenu.js
+++ js/mobilemenu.js
@@ -1,18 +1,20 @@
+//подменю на компах
 $(document).ready(function() {
-    $(".menu_top-item, .navmenu_down-item, .menu_top-link, .down-link").hover(function() { 
+    $(".menu_top-item").hover(function() { 
         $(this).find(".navmenu_down-item").show(400);
     } , function() { 
         $(this).find(".navmenu_down-item").hide();
     });
 });
 
-
+//переключатель кнопки
 $(document).ready(function() {
     $(".menu-icon").click(function() {
         $(this).toggleClass('close-icon');
     });
 });
 
+//адаптивное меню
 $('.menu-icon').click(function() {
   if ($('#top-nav').hasClass('menu')) {
     $('#top-nav').removeClass('menu').addClass('menu-mobile');
@@ -22,7 +24,24 @@ $('.menu-icon').click(function() {
   return false;
 })
 
+//подменю на телефонах
 $(".menu_top-item").click(function(){
-    $(this).find('.navmenu_down-item').slideToggle();
-    return false;
+    $(this).find('.navmenu_down-item').show(); 
 });
+
+
+
+//поиск
+$(document).ready(function() {
+    $(".search-icon").click(function() {
+        $(this).toggleClass('close-search');
+        $(".search-item").find(".navmenu_down-item").toggleClass('down-search');
+        $('.header-search').focus();
+    });
+});
+
+
+
+
+
+
