1c7183cac9717880b7e985aa1cabc539bc43f191
asleep-in-the-deep <47196327+asleep-in-the-deep@users.noreply.github.com>
2019-07-03 20:31:31 +0300

    Add files via upload

--- js/mobilemenu.js
+++ js/mobilemenu.js
@@ -1,10 +1,18 @@
 //подменю на компах
 $(document).ready(function() {
-    $(".menu_top-item").hover(function() { 
-        $(this).find(".navmenu_down-item").show(400);
-    } , function() { 
-        $(this).find(".navmenu_down-item").hide();
-    });
+    var clientWidth = document.body.clientWidth;
+    if (clientWidth > 1025) {
+        $(".menu_top-item").hover(function() { 
+            $(this).find(".navmenu_down-item").show(400);
+        } , function() { 
+            $(this).find(".navmenu_down-item").hide();
+        });
+    }
+    else {
+        $(".menu_top-item").click(function(){
+            $(this).find('.navmenu_down-item').slideToggle();
+        });
+    }
 });
 
 //переключатель кнопки
@@ -19,29 +27,24 @@ $('.menu-icon').click(function() {
   if ($('#top-nav').hasClass('menu')) {
     $('#top-nav').removeClass('menu').addClass('menu-mobile');
   } else {
-    $('#top-nav').removeClass('menu-mobile').addClass('menu');
+    $('#top-nav').removeClass('menu-mobile').addClass('menu').slideUp();
   }
   return false;
 })
 
 //подменю на телефонах
-$(".menu_top-item").click(function(){
-    $(this).find('.navmenu_down-item').show(); 
-});
-
+/*$(document).ready(function() {
+	$(".menu_top-item").click(function(){
+    	$(this).find('.navmenu_down-item').slideDown();
+	});
+});*/
 
 
 //поиск
 $(document).ready(function() {
     $(".search-icon").click(function() {
         $(this).toggleClass('close-search');
-        $(".search-item").find(".navmenu_down-item").toggleClass('down-search');
+        $(".search-item").find(".navmenu_down-item").toggleClass('down-search').animate({height: "50px"});
         $('.header-search').focus();
     });
 });
-
-
-
-
-
-
